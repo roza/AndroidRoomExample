94263f9ef301cd16b0dd792db1ae679b03706955
Gerard Rozsavolgyi <roza@univ-orleans.fr>
2018-02-21 14:48:20 +0100

    added WordRoomDataBase

---
+++ app/src/main/java/fr/example/android/roza/roomexample/WordRoomDatabase.java
@@ -0,0 +1,36 @@
+package fr.example.android.roza.roomexample;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+/**
+ * Created by roza on 21/02/18.
+ */
+
+@Database(entities = {Word.class}, version = 1)
+public abstract class WordRoomDatabase extends RoomDatabase {
+
+      public abstract WordDao wordDao();
+
+    private static WordRoomDatabase INSTANCE;
+
+    static WordRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "word_database")
+                            // Wipes and rebuilds instead of migrating if no Migration object.
+                            // Migration is not part of this codelab.
+                           // .fallbackToDestructiveMigration()
+                            //.addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
